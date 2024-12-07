d71573cce24b242165dd74f918294e8e7b752c4c
Davidavirn <dasamiarea@gmail.com>
2024-12-08 00:01:45 +0100

    chiquifix

--- Excepciones/MalUsoComando/TratoIncompatible.java
+++ Excepciones/MalUsoComando/TratoIncompatible.java
@@ -3,7 +3,7 @@ import monopoly.Valor;
 
 public class TratoIncompatible extends MalUsoComando {
     public TratoIncompatible(String mensaje){
-        super(Valor.RED+" Relacionado con trato"+Valor.RESET+", porque:\n"+ mensaje);
+        super(Valor.RED+" Comando de trato"+Valor.RESET+", porque:\n"+ mensaje);
     }
     
 }
